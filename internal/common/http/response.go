7ea53867a2c94adfdfbfa1ab2d2b897d9d104c9f
Diego Duarte <duartqx@gmail.com>
2025-04-21 17:08:07 -0300

    common: Adição de mimetypes de Content-Types conhecidos

--- internal/common/http/response.go
+++ internal/common/http/response.go
@@ -8,6 +8,16 @@ import (
 	t "github.com/duartqx/livredger/internal/common/types"
 )
 
+const (
+	JSON              = "application/json"
+	HTML              = "text/html; charset=utf-8"
+	HTMX              = "text/htmx"
+	PlainText         = "text/plain; charset=utf-8"
+	XML               = "application/xml"
+	FormURLEncoded    = "application/x-www-form-urlencoded"
+	MultipartFormData = "multipart/form-data"
+)
+
 type Resultado[T any] struct {
 	Total int   `json:"total"`
 	Itens *[]*T `json:"itens"`
