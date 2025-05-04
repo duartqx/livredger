d6baa8f419606c67e67fa45c15885e94bdbe2917
Diego Duarte <duartqx@gmail.com>
2025-05-04 17:45:48 -0300

    feat: Customizado css do AirDatePicker, adiciona subinputs dentro de intervalo-picker para [inicio] e [final]

--- internal/presentation/static/js/components/datepicker.js
+++ internal/presentation/static/js/components/datepicker.js
@@ -1,24 +1,55 @@
 import { locale } from "../utils/locale.js";
 
+/**
+ * @typedef {Object} IntervaloRef
+ * @property {HTMLInputElement} inicio
+ * @property {HTMLInputElement} final
+ * @property {(chave: string, valor: string | Date | undefined) => void} atualiza
+ */
+
 export class IntervaloPicker extends HTMLInputElement {
   constructor() {
     super();
+
+    this.setAttribute("readonly", "true");
+
+    /** @type {IntervaloRef} */
+    this.intervalo = {
+      inicio: this.#criarInnerInputDoIntervalo("inicio"),
+      final: this.#criarInnerInputDoIntervalo("final"),
+      atualiza: function(chave, valor) {
+        this[chave].setAttribute(
+          "value",
+          (valor && dayjs(valor).format("YYYY-MM-DD")) || "",
+        );
+      },
+    };
+
+    this.removeAttribute("name");
 
     if (window.AirDatepicker) {
       this.picker = new window.AirDatepicker(this, {
         range: true,
         multipleDatesSeparator: " - ",
         locale: locale(),
+        position: "bottom center",
+        offset: -1,
         onSelect: ({ date, formattedDate, datepicker }) => {
-          console.log(formattedDate)
-          this.intervalo = {
-            inicio: date[0] && dayjs(date[0]).format("YYYY-MM-DD") || "",
-            final: date[1] && dayjs(date[1]).format("YYYY-MM-DD") || "",
-          };
-          console.log("Intervalo", this.intervalo)
-          this.dispatchEvent(new Event("change", { bubbles: true }));
+          this.intervalo.atualiza("inicio", date[0]);
+          this.intervalo.atualiza("final", date[1]);
         },
       });
     }
+  }
+  /** @returns {HTMLInputElement} */
+  #criarInnerInputDoIntervalo(nome) {
+    const membro = document.createElement("input");
+
+    membro.setAttribute("hidden", true);
+    membro.setAttribute("name", `${this.getAttribute("name")}[${nome}]`);
+
+    this.append(membro);
+
+    return membro;
   }
 }
