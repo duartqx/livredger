f6ec6ead3259a7c3f11f3191d96fc17c443e2932
Diego Duarte <duartqx@gmail.com>
2025-06-30 00:21:32 -0300

    Nomes nos genericos de Resultado

--- internal/application/resultado.go
+++ internal/application/resultado.go
@@ -1,25 +1,30 @@
 package application
 
-type Enumerated[T any] struct {
+type Enumerated[Item any] struct {
 	Index   int
-	Item    T
+	Item    Item
 	IsFirst bool
 	IsLast  bool
 }
 
-type Resultado[C, T any] struct {
-	Total    int   `json:"total"`
-	Consulta *C    `json:"consulta"`
-	Itens    *[]*T `json:"itens"`
+type Resultado[Consulta, Item any] struct {
+	Total    int       `json:"total"`
+	Consulta *Consulta `json:"consulta"`
+	Itens    *[]*Item  `json:"itens"`
 }
 
-func (r Resultado[C, T]) Enumerated() *[]Enumerated[T] {
-	enumerated := []Enumerated[T]{}
+func (r Resultado[Consulta, Item]) Enumerated() *[]Enumerated[Item] {
+	enumerated := []Enumerated[Item]{}
 
 	for index, item := range *r.Itens {
 		enumerated = append(
 			enumerated,
-			Enumerated[T]{Index: index, Item: *item, IsFirst: index == 0, IsLast: index == r.Total-1},
+			Enumerated[Item]{
+				Index:   index,
+				Item:    *item,
+				IsFirst: index == 0,
+				IsLast:  index == r.Total-1,
+			},
 		)
 	}
 
