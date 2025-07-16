dc237155b26a0950559d0c827094ecdca774c38e
Diego Duarte <duartqx@gmail.com>
2025-07-15 22:36:15 -0300

    tratador LancamentoContaCriada passa evento.Timestamp como lancamento.Vencimento

--- internal/application/services/executores/eventos.go
+++ internal/application/services/executores/eventos.go
@@ -7,6 +7,8 @@ import (
 	"github.com/duartqx/livredger/internal/domain/comandos"
 	"github.com/duartqx/livredger/internal/domain/eventos"
 	"github.com/duartqx/livredger/internal/domain/mensagens"
+	"github.com/duartqx/livredger/internal/domain/value/meios"
+	"github.com/duartqx/livredger/internal/domain/value/naturezas"
 	"github.com/duartqx/livredger/internal/infra"
 )
 
@@ -24,9 +26,10 @@ func LancamentoContaCriada(uow infra.UnidadeDeTrabalho, mensagem any) error {
 		Chave:          evento.Chave,
 		Versao:         1,
 		Valores:        0,
-		Natureza:       "Outro",
-		MeioFinanceiro: "Outro",
+		Natureza:       naturezas.OUTRO,
+		MeioFinanceiro: meios.OUTRO,
 		Descricao:      "Abertura da Conta",
+		Vencimento:     evento.Timestamp,
 	})
 
 	return err
